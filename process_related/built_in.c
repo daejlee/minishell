853e71c7705d13fc6a9b4c14fcb1ff9806e70104
이대진 <idaejin@idaejin-ui-MacBookPro.local>
2022-12-05 17:35:44 +0900

    built_in function cd pseudo code implemented.

--- process_related/built_in.c
+++ process_related/built_in.c
@@ -1,6 +1,6 @@
 #include "../minishell.h"
 
-/* 구현해야 하는 쉘 빌트인 함수들을 열거형을 이용해 나열했습니다 */
+/*	구현해야 하는 쉘 빌트인 함수들을 열거형을 이용해 나열했습니다	*/
 enum	built_in_code
 {
 	echo = 1,
@@ -13,7 +13,7 @@ enum	built_in_code
 };
 
 /*	빌트인 echo 함수입니다. -n 옵션을 지원합니다.
-	original : echo [-n] [string ...] */
+	original : echo [-n] [string ...]	*/
 int	ft_echo(char *buf, int n_option)
 {
 	int		write_ret;
@@ -31,6 +31,38 @@ int	ft_echo(char *buf, int n_option)
 		return (1);
 	else
 		return (0);
+}
+
+/*	빌트인 cd 함수입니다. 상대 경로 혹은 절대 경로만을 사용합니다.
+	original : cd [-L|-P] [directory]	*/
+int	ft_cd(char *dir)
+{
+	char	*curpath;
+
+	if (!dir && ($HOME is empty || undefined))	//1st
+		return (0);
+	else if (!dir)	//2nd
+		dir = $HOME;
+	if (dir[0] == '/')	//3rd
+	{
+		curpath = dir;
+		GOTO 7th;
+	}
+	else if (!ft_strncmp(dir, "..", 2) || dir[0] == '.')	//4th
+		GOTO 6th;
+	if (!get_cdpath())	// 5th
+		GOTO 7th;
+	else	//6th
+		curpath = dir;
+	if (curpath[0] != '/')	//7th
+	{
+		if (PWD dont end with '/')
+			PWD = ft_strjoin(PWD, '/');
+		curpath = ft_strjoin(PWD, curpath);
+	}
+	get_canonical_curpath();	//8th
+	trim_curpath();	//9th
+	return (chdir(curpath));	//10th
 }
 
 /* 들어온 커맨드가 빌트인 함수를 호출하는 지 판단하는 함수입니다. */
