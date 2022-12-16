1488dccff495981aa9369a310fce53b4b67d66c5
이대진 <idaejin@idaejin-ui-MacBookPro.local>
2022-12-16 15:56:08 +0900

    feature : 실행부 히어도큐먼트 제외한 초안 완성

--- execve_related/process.h
+++ execve_related/process.h
@@ -15,6 +15,7 @@ typedef struct s_pcs
 	int		*pfd;
 	int		*next_pfd;
 	char	**com;
+	char	**envp;
 	pid_t	*pids;
 }	t_pcs;
 
@@ -28,7 +29,7 @@ int			err_terminate(t_pcs *p);
 char		*ft_strjoin_modified(char const *s1, char const *s2);
 char		**ft_split_awk(char *s);
 int			wait_for_children(t_pcs *p, pid_t *pids, int temp);
-void		exec_com(t_pcs *p, t_token *now, int i);
+void		exec_com(t_pcs *p, t_token *now, int i, t_env *env);
 int			here_doc(t_pcs *p, int argc, char *argv[]);
 void		exec_sh(t_pcs *p, char *argv[], int i);
 
