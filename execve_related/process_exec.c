8ccb2fce9d7a4926999a552804a724b35907d526
Lee Daejin <94844819+savif4991@users.noreply.github.com>
2022-12-30 14:29:48 +0900

    Style: here_doc 부분 readline으로 논리 변경

--- execve_related/process_exec.c
+++ execve_related/process_exec.c
@@ -93,43 +93,26 @@ int	here_doc_seg(t_pcs *p, t_token *now)
 	char	*ret;
 	char	*temp;
 	int		fst_flag;
-	char	buffer[10];
-	char	*prev_nl;
+	char	*buf;
 
-	limiter = ft_strjoin(now->next->str, "\n");
-	if (!limiter)
-		return (1);
+	limiter = now->next->str;
 	unlink(HERE_DOC_INPUT_BUFFER);
 	here_doc_fd = open(HERE_DOC_INPUT_BUFFER, O_WRONLY | O_TRUNC | O_CREAT, 0644);
 	if (here_doc_fd == -1)
-	{
-		free (limiter);
 		return (1);
-	}
 	ret = "";
-	prev_nl = NULL;
 	fst_flag = 1;
 	while (!ft_strnstr(ret, limiter, ft_strlen(ret)))
 	{
-		write(1, ">", 1);
-		while (1)
-		{
-			read(0, buffer, 10);
-			temp = ret;
-			ret = ft_strjoin(ret, buffer);
-			if (!fst_flag)
-				free(temp);
-			if (ft_strrchr(ret, '\n') && ft_strrchr(ret, '\n') != prev_nl)
-			{
-				*(ft_strrchr(ret, '\n') + 1) = '\0';
-				prev_nl = ft_strchr(ret, '\n');
-				break ;
-			}
-			fst_flag = 0;
-		}
+		buf = readline(">");
+		temp = ret;
+		ret = ft_strjoin(ret, buf);
+		free(buf);
+		if (!fst_flag)
+			free(temp);
+		fst_flag = 0;
 	}
 	*ft_strnstr(ret, limiter, ft_strlen(ret)) = '\0';
-	free (limiter);
 	write(here_doc_fd, ret, ft_strlen(ret));
 	close(here_doc_fd);
 	here_doc_fd = open(HERE_DOC_INPUT_BUFFER, O_RDONLY);
