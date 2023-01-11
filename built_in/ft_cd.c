67edb01caa20301c6745776dcd8c40d55cf75431
Daejin Lee <daejlee@c4r2s8.42seoul.kr>
2023-01-11 11:02:55 +0900

    Fix: ft_cd 노미넷 수정

--- built_in/ft_cd.c
+++ built_in/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: daejlee <daejlee@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/10 14:28:14 by daejlee           #+#    #+#             */
-/*   Updated: 2023/01/11 10:59:16 by daejlee          ###   ########.fr       */
+/*   Updated: 2023/01/11 11:02:30 by daejlee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,8 +46,7 @@ static int	set_1(t_ft_cd *p, char **dir_adr, t_env *env)
 	p->env_cdpath = get_env_val("CDPATH", env);
 	p->env_pwd = get_env_val("PWD", env);
 	p->node = find_env(env, "OLDPWD");
-
-	if(!set_3(p, dir_adr))
+	if (!set_3(p, dir_adr))
 		return (0);
 	if (p->node)
 	{
